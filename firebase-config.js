f8945c5c665e7a356c55f1ae842404bd36b5d463
leonardoyzg <leonardoyaelzg02@gmail.com>
2026-06-03 15:28:48 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCLasH4PAkNgtNlpio_1BIZ9xiHReC_Qqs",
+
+    authDomain: "app-nimbus.firebaseapp.com",
+
+    databaseURL: "https://app-nimbus-default-rtdb.firebaseio.com",
+
+    projectId: "app-nimbus",
+
+    storageBucket: "app-nimbus.firebasestorage.app",
+
+    messagingSenderId: "537012946123",
+
+    appId: "1:537012946123:web:b9241c2b0441e1ad0c074e",
+
+    measurementId: "G-03Y07CMJB9"
+};
+
+// INICIALIZAR
+const app = initializeApp(firebaseConfig);
+
+// AUTH
+export const auth = getAuth(app);
